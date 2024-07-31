e913d5c391a0c9e86e0d479fda7700c27571b6b8
Leon Kwan <kwan.leon@gmail.com>
2024-07-30 20:57:32 -0700

    date picker logic

--- src/routes/create-event/+page.svelte
+++ src/routes/create-event/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
-	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const daysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 	const today = new Date();
 	const nextDates = [...Array(28).keys()].map(
 		(i) => new Date(today.getFullYear(), today.getMonth(), today.getDate() + i - today.getDay())
 	);
+	let selectedDates: Date[] = [];
+	const handleOnClickDates = (date: Date) => {
+		selectedDates.includes(date)
+			? (selectedDates = [...selectedDates.filter((d) => d !== date)])
+			: (selectedDates = [...selectedDates, date]);
+	};
 </script>
 
-<div class="m-10">
-	<div class="grid grid-cols-7 gap-2 max-w-50">
+<div class="flex justify-left m-10">
+	<div class="grid grid-cols-7 gap-2">
 		{#each daysOfWeek as day}
-			<div class="mx-1">{day}</div>
+			<div class="mx-1 flex justify-center">{day}</div>
 		{/each}
 		{#each nextDates as date}
-			<div class="{date.getDate() == today.getDate() ? 'font-bold' : 'font-normal'} bg-gray-200">
+			<div
+				on:click={() => handleOnClickDates(date)}
+				class="{date.getDate() == today.getDate()
+					? 'font-bold'
+					: 'font-normal'} {selectedDates.includes(date)
+					? 'bg-primary'
+					: 'bg-white hover:bg-primary'} flex justify-center hover:cursor-pointer"
+			>
 				{date.getDate()}
 			</div>
 		{/each}
 	</div>
-	<a href="/">home</a>
 </div>
